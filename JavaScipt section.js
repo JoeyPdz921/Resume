819743875462c6539255bc66f7d9f68e4d265151
Joseph Pedraza <joeypedraza@gmail.com>
2025-11-22 21:59:17 -0500

    Delete JavaScipt section.js

--- JavaScipt section.js
+++
@@ -1,8 +0,0 @@
-console.log("JavaScript is linked properly!");
-button = document.getElementById("myButton");
-button.addEventListener("click", function() {
-    alert("Button was clicked!");
-});
-// You can add more JavaScript code here to enhance your portfolio page.
-
-window.alert("Welcome to my portfolio page!");
