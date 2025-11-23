b60f1316423bab0435dae51e64c9fd510d455302
Joseph Pedraza <joeypedraza@gmail.com>
2025-11-22 22:00:19 -0500

    Add files via upload

---
+++ JavaScipt section.js
@@ -0,0 +1,14 @@
+console.log("JavaScript is linked properly!");
+button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+    alert("Button was clicked!");
+});
+// You can add more JavaScript code here to enhance your portfolio page.
+
+window.alert("Welcome to my portfolio page!");
+console.log("JavaScript is linked properly!");
+button = document.getElementById("myButton");
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
